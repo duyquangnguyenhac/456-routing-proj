cb8f386496ac4a2462777fd37d3b18f803bb2d9c
James <duyquangnguyenhac@gmail.com>
2022-11-29 20:39:26 -0500

    Trying full data_set

--- cvrp.py
+++ cvrp.py
@@ -11,7 +11,11 @@ def parse_demands():
     # demands are represented by dict where key is the ref number. the number of demands in the list
     # represents how many times it needs to be delivered each month, and how many pallets in each turn.
     excel_data = pd.read_excel(r'FBWMLocationsDemands.xlsx')
-    pallets_demands = defaultdict(list)
+    extra_nodes_idx = 137
+    ref_to_nodes_idx = defaultdict(list)
+    pallets_demands = {0: 0}
+    idx_to_ref = {}
+    idx_to_ref[0] = 0
     for idx in range(1, 137):
         # 0: Ref number, 1: Name, 6: Number of pallets, 7: monthly demands
         ref, num_pals, freq = excel_data.iloc[idx][0], excel_data.iloc[idx][6], excel_data.iloc[idx][7]
@@ -31,15 +35,24 @@ def parse_demands():
             num_pals = [2, 1]
         else:
             num_pals = num_pals
-
+        nodes = []
         for i in range(num_times):
             if type(num_pals) == list:
-                pallets_demands[ref].append(num_pals[0])
+                cur_demand = num_pals[0]
                 num_pals.pop(0)
             else:
-                pallets_demands[ref].append(num_pals)
+                cur_demand = num_pals
 
-    return pallets_demands
+            if i == 0:
+                pallets_demands[idx] = cur_demand
+                idx_to_ref[idx] = idx
+            elif i > 0:
+                pallets_demands[extra_nodes_idx] = cur_demand
+                idx_to_ref[extra_nodes_idx] = idx
+                extra_nodes_idx += 1  
+        # ref_to_nodes_idx[ref] = nodes
+    return pallets_demands, idx_to_ref
+
 class DataModel:
     def __init__(self):
         # HOW TO load a json file as python object
@@ -55,8 +68,9 @@ class DataModel:
             
         #Demands
         #Dict containing node i and n_th constraints representing times it in a month
-        self._demands = parse_demands()
-        self._demands = [0, 4, 4, 6]
+        pallet_demands, idx_to_ref = parse_demands()
+        self._demands = pallet_demands
+        self._idx_to_ref = idx_to_ref
 
     def _set_travel_matrix(self, travel_time_matrix):
         self._travel_times = travel_time_matrix
@@ -64,11 +78,9 @@ class DataModel:
 def run(data):
     # data = DataModel()
     m = gp.Model()
-    num_of_nodes = 4
     Q = 8
-    # for i in range(3):
-    #     monthly_demands = len(data._demands[i])
-    #     num_of_nodes += monthly_demands
+
+    num_of_nodes = len(data._demands)
 
     x = m.addMVar((num_of_nodes, num_of_nodes), vtype=GRB.INTEGER, lb=0, name="x")
     K = m.addVar(vtype=GRB.INTEGER, lb=0, name="K")
@@ -84,7 +96,8 @@ def run(data):
         cur_sum_enters = 0
         m.addConstr(x[i,i] == 0)
         for j in range(num_of_nodes):
-            if i == j or data._distances[i][j] == 0: 
+            # If j references the same node, we shouldn't include this edge.
+            if i == j or (data._idx_to_ref[j] == i): 
                 continue
             cur_sum_enters += x[i, j]
         m.addConstr(cur_sum_enters == 1)
@@ -92,27 +105,31 @@ def run(data):
     for j in range(1, num_of_nodes):
         cur_sum = 0
         for i in range(num_of_nodes):
-            if i == j or data._distances[i][j] == 0: continue
+            # If j references the same node, we shouldn't include this edge.
+            if i == j or (data._idx_to_ref[j] == i): continue
             cur_sum += x[i, j]
         m.addConstr(cur_sum == 1)
 
     for i in range(1, num_of_nodes):
         for j in range(1, num_of_nodes):
             if i == j: continue
+            get_demand = data._demands[j]
             m.addConstr(u[i] - u[j] + Q * x[i,j] <= Q - data._demands[j])
 
     for i in range(1, num_of_nodes):
         m.addConstr(data._demands[i] <= u[i])
         m.addConstr(u[i] <= Q)
 
-    # for i in range(num_of_nodes):
-    #     for j in range(num_of_nodes):
-    #         m.addConstr((x[i,j] == 0) >> data._distances[i][j] * x[i,j] >= 0)
-
     cumul_sum = 0
     for i in range(num_of_nodes):
         for j in range(num_of_nodes):
-            cumul_sum += data._travel_times[i][j] * x[i,j]
+            ref_num = data._idx_to_ref[i]
+            dest_ref_num = data._idx_to_ref[j]
+            if ref_num == dest_ref_num:
+                travel_time = 0
+            else:
+                travel_time = data._travel_times[ref_num][dest_ref_num]
+            cumul_sum += travel_time * x[i,j]
 
     m.setObjective(cumul_sum, GRB.MINIMIZE)
     m.optimize()
@@ -129,31 +146,34 @@ def run(data):
             print(v.varName, "=", v.x)
         
 if __name__ == "__main__":
-    print("BEGIN TESTS =>")
-    # Test Case 1
+    # print("BEGIN TESTS =>")
+    # # Test Case 1
+    # data = DataModel()
+    # # Depot on the Side
+    # travel_times = [[0, 4, 7, 7], [4, 0, 3, 3], [7, 3, 0, 0], [7, 3, 0, 0]]
+    # data._set_travel_matrix(travel_times)
+    # run(data)
+
+    # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    # # Test Case 2
+    # # Depot in the middle
+    # travel_times = [[0, 4, 3, 3], [4, 0, 7, 7], [3, 7, 0, 0], [3, 7, 0, 0]]
+    # data._set_travel_matrix(travel_times)
+    # run(data)
+
+    # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    # # Test Case 3 
+    # # Depot in the middle shifted down a little 
+    # travel_times = [[0, 4, 3, 3], [4, 0, 6, 6], [3, 6, 0, 0], [3, 6, 0, 0]]
+    # data._set_travel_matrix(travel_times)
+    # run(data)
+
+    # print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    # # Test Case 4
+    # # Depot in the middle shifted down a little 
+    # travel_times = [[0, 4, 3, 3], [4, 0, 6, 6], [3, 6.5, 0, 0], [3, 6.5, 0, 0]]
+    # data._set_travel_matrix(travel_times)
+    # run(data)
+
     data = DataModel()
-    # Depot on the Side
-    travel_times = [[0, 4, 7, 7], [4, 0, 3, 3], [7, 3, 0, 0], [7, 3, 0, 0]]
-    data._set_travel_matrix(travel_times)
-    run(data)
-
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    # Test Case 2
-    # Depot in the middle
-    travel_times = [[0, 4, 3, 3], [4, 0, 7, 7], [3, 7, 0, 0], [3, 7, 0, 0]]
-    data._set_travel_matrix(travel_times)
-    run(data)
-
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    # Test Case 3 
-    # Depot in the middle shifted down a little 
-    travel_times = [[0, 4, 3, 3], [4, 0, 6, 6], [3, 6, 0, 0], [3, 6, 0, 0]]
-    data._set_travel_matrix(travel_times)
-    run(data)
-
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    # Test Case 4
-    # Depot in the middle shifted down a little 
-    travel_times = [[0, 4, 3, 3], [4, 0, 6, 6], [3, 6.5, 0, 0], [3, 6.5, 0, 0]]
-    data._set_travel_matrix(travel_times)
     run(data)
